39bbb5281729ae66ab27afd241f2e61616bf346a
Allison Howard <alliemhow@gmail.com>
2019-08-27 06:35:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ BoardMember.prototype.veto = function(){
 BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
-BoardMember.prototype..doCharity = function(){
+BoardMember.prototype.doCharity = function(){
   return "I like to help people."
 }
 //
